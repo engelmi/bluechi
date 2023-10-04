729c371d37d31b5e54b645534ea08e92c1e2bec7
Michael Engel <mengel@redhat.com>
2023-10-04 12:48:46 +0200

    fix invalid read in node disconnect
    Signed-off-by: Michael Engel <mengel@redhat.com>

--- src/manager/node.c
+++ src/manager/node.c
@@ -948,6 +948,7 @@ static int node_disconnected(UNUSED sd_bus_message *message, void *userdata, UNU
         /* Remove anonymous nodes when they disconnect */
         if (node->name == NULL) {
                 manager_remove_node(manager, node);
+                bc_log_info("Anonymous node disconnected");
         } else {
                 /* Remove all jobs associated with the registered node that got
                    disconnected. */
@@ -962,13 +963,8 @@ static int node_disconnected(UNUSED sd_bus_message *message, void *userdata, UNU
                                 }
                         }
                 }
-                node_unset_agent_bus(node);
-        }
-
-        if (node->name) {
                 bc_log_infof("Node '%s' disconnected", node->name);
-        } else {
-                bc_log_info("Anonymous node disconnected");
+                node_unset_agent_bus(node);
         }
 
         return 0;
